e568da4206e6c7064852ffeb394972f372ef1f96
v.bednyakov <v.bednyakov@cloudcom.ru>
2024-06-23 14:04:59 +0300

    fix create orders

--- bot.py
+++ bot.py
@@ -2,14 +2,16 @@
 
 import telebot
 from telebot import types
+from telebot.types import ReplyKeyboardRemove
 from envs import TOKEN, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST
 import psycopg2
+import re
 
 
 bot = telebot.TeleBot(TOKEN)
 
 users = {
-            'admins': [424488454], 
+            'admins': [424488454, 798583829], 
             'users': []
         }
 
@@ -103,37 +105,6 @@ def create_order(message):
     else:
         bot.send_message(message.chat.id, "Permission denied!")
 
-def question_size(message):
-    global select_type
-    select_type = message.text
-    sent = bot.send_message(message.chat.id, "Размер АКБ(мм)")
-    bot.register_next_step_handler(sent, question_voltage)
-
-def question_voltage(message):
-    global select_size
-    select_size = message.text
-    sent = bot.send_message(message.chat.id, "Напряжение(nom(V))")
-    bot.register_next_step_handler(sent, question_lenght)
-
-def question_lenght(message):
-    global select_voltage
-    select_voltage = message.text
-    sent = bot.send_message(message.chat.id, "Укажите длину выводов(мм)")
-    bot.register_next_step_handler(sent, question_name)
-
-def question_name(message):
-    global select_lenghts
-    select_lenghts = message.text
-    sent = bot.send_message(message.chat.id, "ФИО")
-    bot.register_next_step_handler(sent, question_number_phone)
-
-def question_number_phone(message):
-    global select_FIO
-    select_FIO = message.text
-    sent = bot.send_message(message.chat.id, "Укажите номер телефона (в формате 7XXXXXXXXX)")
-    bot.register_next_step_handler(sent, handle_confirmation)
-
-
 '''def define_size(message):
     global select_type
     select_type = message.text
@@ -143,20 +114,74 @@ def question_number_phone(message):
     sent = bot.send_message(message.chat.id, "Выберите размер аккумулятора:", reply_markup=buttons_size)
     bot.register_next_step_handler(sent, handle_confirmation)'''
 
+def question_size(message):
+    global select_type
+    select_type = message.text
+    sent = bot.send_message(message.chat.id, "Размер АКБ(мм)", reply_markup=ReplyKeyboardRemove())
+    bot.register_next_step_handler(sent, question_voltage)
+
+def question_voltage(message):
+    global select_size
+    select_size = message.text
+    if (re.match("^[0-9]+[x][0-9]+", select_size.lower()) or re.match("^[0-9]+[х][0-9]+", select_size.lower()) ):
+#        print("букв нет")
+        sent = bot.send_message(message.chat.id, "Напряжение(nom(V))")
+        bot.register_next_step_handler(sent, question_lenght)
+    else:
+        print("тут есть буквы")
+        sent = bot.send_message(message.chat.id, f"Допустимый формат записи ЧИСЛОхЧИСЛО (например 300х200).\nПовторите попытку.")
+        bot.register_next_step_handler(sent, question_voltage)
+
+def question_lenght(message):
+    global select_voltage
+    select_voltage = message.text
+    if re.match("^[0-9,.]+$", select_voltage):
+        print("chars not found")
+        sent = bot.send_message(message.chat.id, "Укажите длину выводов(мм)")
+        bot.register_next_step_handler(sent, question_name)
+    else:
+        sent = bot.send_message(message.chat.id, f"Неверный формат записи.\nНеобходимо указывать без единиц измерения (только число).\nПовторите попытку.")
+        bot.register_next_step_handler(sent, question_lenght)
+
+def question_name(message):
+    global select_lenghts
+    select_lenghts = message.text
+    if re.match("^[0-9,.]+$", select_lenghts):
+        print("chars not found")
+        sent = bot.send_message(message.chat.id, "ФИО")
+        bot.register_next_step_handler(sent, question_number_phone)
+    else:
+        sent = bot.send_message(message.chat.id, f"Неверный формат записи.\nНеобходимо указывать без единиц измерения (только число).\nПовторите попытку.")
+        bot.register_next_step_handler(sent, question_name)
+
+def question_number_phone(message):
+    global select_FIO
+    select_FIO = message.text
+    sent = bot.send_message(message.chat.id, "Укажите номер телефона (в формате 7XXXXXXXXX)")
+    bot.register_next_step_handler(sent, handle_confirmation)
+
+
+
 def handle_confirmation(message):
     global select_number_phone
     select_number_phone = message.text
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton('Принять'), types.KeyboardButton('Отменить'))
-    message_confirmation=f"Ваш заказ:\nТип ячеек: {select_type}\nДлина выводов: {select_lenghts}мм\nНапряжение: {select_voltage}V\nРазмер АКБ: {select_size}\nИмя клиента: {select_FIO}\nНомер телефона: {select_number_phone}\nНажмите 'Принять' для подтверждения или 'Отменить' для отмены."
-    sent = bot.send_message(message.chat.id, message_confirmation, reply_markup=markup)
-    #bot.send_message(message.chat.id, f"Тип {select_type} размер {select_size}")
-    bot.register_next_step_handler(sent, result_message)
+    if (re.match("[0-9]+", select_number_phone) and len(select_number_phone)<=11):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton('Принять'), types.KeyboardButton('Отменить'))
+        message_confirmation=f"Тип ячеек: {select_type}\nДлина выводов: {select_lenghts} мм\nНапряжение: {select_voltage} V\nРазмер АКБ: {select_size} мм\nИмя клиента: {select_FIO}\nНомер телефона: {select_number_phone}\nНажмите 'Принять' для подтверждения или 'Отменить' для отмены."
+        sent = bot.send_message(message.chat.id, message_confirmation, reply_markup=markup)
+        #bot.send_message(message.chat.id, f"Тип {select_type} размер {select_size}")
+        bot.register_next_step_handler(sent, result_message)
+    else:
+        sent = bot.send_message(message.chat.id, f"Неверный формат записи.\nПовторите попытку.")
+        bot.register_next_step_handler(sent, handle_confirmation)
 
 def result_message(message):
     if message.text == 'Принять':
         #cursor.execute("insert into public.alfa (operation, price, uptime) values (%s, %s, current_date)", ('ПОПОЛНЕНИЕ', in_price[1:]))
-        cursor.execute("INSERT INTO public.orders (type_battery, size_battery, createdate) VALUES (%s, %s, date_trunc('second', now()) )", (select_type, select_size))
+        cursor.execute("INSERT INTO public.orders (type_battery, size_battery, voltage_battery, lenghts_battery, FIO, number_phone, createdate) \
+                       VALUES (%s, %s, %s, %s, %s, %s, date_trunc('second', now()) )", \
+                       (select_type, select_size, int(select_voltage), int(select_lenghts), select_FIO, int(select_number_phone)))
         conn.commit()
         bot.send_message(message.chat.id, "Спасибо! Ваш заказ принят!")
     elif message.text == 'Отменить':
@@ -171,15 +196,20 @@ def insert_db(typ, size):
 @bot.message_handler(func=lambda message: message.text == 'Посмотреть активные заказы')
 def list_orders(message):
     if (message.from_user.id in users['users']) or (message.from_user.id in users['admins']):
-        cursor.execute("SELECT * FROM public.orders")
+        cursor.execute("SELECT order_id, type_battery, size_battery, voltage_battery, lenghts_battery, FIO, number_phone, createdate \
+                       FROM public.orders")
         orders = cursor.fetchall()
         print(orders)
         for order in orders:
             order_id = order[0]
             type_battery = order[1]
-            size_battery = order[2] 
-            createdate = order[3]
-            bot.send_message(message.chat.id, f"Номер заказа: {order_id}\nТип: {type_battery}\nРазмер: {size_battery}\nДата создания: {createdate}")
+            size_battery = order[2]
+            voltage_battery = order[3] 
+            lenghts_battery = order[4]
+            FIO = order[5]
+            number_phone = order[6]
+            createdate = order[7]
+            bot.send_message(message.chat.id, f"Номер заказа: {order_id}\nТип ячеек: {type_battery}\nРазмер АКБ: {size_battery}мм\nНапряжение: {voltage_battery}V\nДлина выводов: {lenghts_battery}мм\nФИО: {FIO}\nНомер телефона: {number_phone}\nДата создания заказа: {createdate}")
         conn.commit()
     else:
         bot.send_message(message.chat.id, "Permission denied!")
--- sql/1.sql
+++ sql/1.sql
@@ -2,5 +2,13 @@ CREATE TABLE public.orders (
 	order_id    serial primary key,
 	type_battery varchar(40) NULL,
 	size_battery varchar(40) NULL,
+	voltage_battery int4 null,
+	lenghts_battery int4 null,
+	FIO	varchar(40) null,
+	number_phone varchar(40) null,
 	createdate date NULL
+);
+
+CREATE TABLE public.elements (
+	element_type varchar(40) not NULL
 );
